ba2e3b97143ddf520bcbf4e2da9538c874c40c20
V11che <V11che@users.noreply.github.com>
2018-07-25 13:56:21 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,7 +4,7 @@ class minecraft {
   }
   file {'/opt/minecraft/minecraft_server.jar':
     ensure => file,
-    source => https://launcher.mojang.com/mc/game/1.13/server/d0caafb8438ebd206f99930cfaecfa6c9a13dca0/server.jar,
+    source => 'https://launcher.mojang.com/mc/game/1.13/server/d0caafb8438ebd206f99930cfaecfa6c9a13dca0/server.jar',
   }
   package {'java':
     ensure => present,
